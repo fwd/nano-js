28ec6fd4e793ebb5560775dc65db9875bebd96f7
Esteban <esteban@fwd.dev>
2024-05-25 11:21:33 -0400

    1.5.5

--- nano.js
+++ nano.js
@@ -363,7 +363,7 @@ let nano = {
 				  headers: { 
 				  	'content-type': 'application/json', 
 				  	'authorization': this.rpc_key,
-				  	'nano-app': `@nano/wallet-1.5.4`,
+				  	'nano-app': `@nano/wallet-1.5.5`,
 				  }
 				};
 
